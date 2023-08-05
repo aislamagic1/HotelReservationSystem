0cc5ad79d1598c26ce4afb86dc51237a91588c26
aislamagic1 <115921908+aislamagic1@users.noreply.github.com>
2023-08-05 23:49:48 +0200

    Implemented Guests domain class

--- src/main/java/ba/unsa/etf/rpr/domain/Guests.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Guests.java
@@ -1,9 +1,82 @@
 package ba.unsa.etf.rpr.domain;
 
+import java.util.Objects;
+
 /**
  * Bean for Guest
  *
  * @author Aldin Islamagic
  */
 public class Guests {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Guests{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Guests guests = (Guests) o;
+        return id == guests.id && Objects.equals(firstName, guests.firstName) && Objects.equals(lastName, guests.lastName) &&
+                Objects.equals(email, guests.email) && Objects.equals(password, guests.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, email, password);
+    }
 }
