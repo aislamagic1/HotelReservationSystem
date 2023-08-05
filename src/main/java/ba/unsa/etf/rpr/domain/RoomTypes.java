f0c9b97fe00766017d42b9942ada2b41aece0db7
aislamagic1 <115921908+aislamagic1@users.noreply.github.com>
2023-08-05 23:56:58 +0200

    Implemented RoomTypes domain class

--- src/main/java/ba/unsa/etf/rpr/domain/RoomTypes.java
+++ src/main/java/ba/unsa/etf/rpr/domain/RoomTypes.java
@@ -1,5 +1,7 @@
 package ba.unsa.etf.rpr.domain;
 
+import java.util.Objects;
+
 /**
  * List of types of rooms
  *
@@ -7,5 +9,63 @@ package ba.unsa.etf.rpr.domain;
  */
 
 public class RoomTypes {
+    private int id;
+    private String roomType;
+    private int numPersons;
+    private double roomPrice;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public int getNumPersons() {
+        return numPersons;
+    }
+
+    public void setNumPersons(int numPersons) {
+        this.numPersons = numPersons;
+    }
+
+    public double getRoomPrice() {
+        return roomPrice;
+    }
+
+    public void setRoomPrice(double roomPrice) {
+        this.roomPrice = roomPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "RoomTypes{" +
+                "id=" + id +
+                ", roomType='" + roomType + '\'' +
+                ", numPersons=" + numPersons +
+                ", roomPrice=" + roomPrice +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RoomTypes roomTypes = (RoomTypes) o;
+        return id == roomTypes.id && numPersons == roomTypes.numPersons && Double.compare(roomTypes.roomPrice, roomPrice) == 0 && Objects.equals(roomType, roomTypes.roomType);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, roomType, numPersons, roomPrice);
+    }
 }
