75109e3d6ca468d280183b8af4e23d91fd11ab87
aislamagic1 <115921908+aislamagic1@users.noreply.github.com>
2023-08-06 15:07:26 +0200

    Added constructor for ReservationsDaoSQLImpl

--- src/main/java/ba/unsa/etf/rpr/dao/ReservationsDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/ReservationsDaoSQLImpl.java
@@ -2,9 +2,31 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Reservations;
 
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.List;
+import java.util.Properties;
 
 public class ReservationsDaoSQLImpl implements ReservationsDao{
+
+    private Connection connection;
+
+    /**
+     * Constructor that makes connection to the database with  hidden values needed to make the connection
+     */
+    public ReservationsDaoSQLImpl(){
+        try{
+            String fieldPath = "src/dataBase.properties";
+            Properties pros = new Properties();
+            FileInputStream ip = new FileInputStream(fieldPath);
+            pros.load(ip);
+            this.connection = DriverManager.getConnection(pros.getProperty("url"), pros.getProperty("username"), pros.getProperty("password"));
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public Reservations getById(int id) {
         return null;
