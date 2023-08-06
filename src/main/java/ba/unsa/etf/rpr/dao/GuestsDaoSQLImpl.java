3caeb8d402bd3087648b4c6ba7eabc44c89fe018
aislamagic1 <115921908+aislamagic1@users.noreply.github.com>
2023-08-06 11:58:32 +0200

    Added constructor to GuestsDaoSQLImpl

--- src/main/java/ba/unsa/etf/rpr/dao/GuestsDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/GuestsDaoSQLImpl.java
@@ -2,9 +2,33 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Guests;
 
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.Properties;
 
 public class GuestsDaoSQLImpl implements GuestsDao{
+
+    private Connection connection;
+
+
+    /**
+     * Constructor that makes connection to the database with  hidden values needed to make the connection
+     */
+    public GuestsDaoSQLImpl(){
+        try{
+            String fieldPath = "src/dataBase.properties";
+            Properties pros = new Properties();
+            FileInputStream ip = new FileInputStream(fieldPath);
+            pros.load(ip);
+            this.connection = DriverManager.getConnection(pros.getProperty("url"), pros.getProperty("username"), pros.getProperty("password"));
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public Guests getById(int id) {
