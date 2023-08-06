d5d856c84b9f38e73239750da20855789876296f
aislamagic1 <115921908+aislamagic1@users.noreply.github.com>
2023-08-06 21:24:18 +0200

    Added constructor in RoomTypesDaoSQLImpl

--- src/main/java/ba/unsa/etf/rpr/dao/RoomTypesDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/RoomTypesDaoSQLImpl.java
@@ -2,9 +2,31 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.RoomTypes;
 
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.List;
+import java.util.Properties;
 
 public class RoomTypesDaoSQLImpl implements RoomTypesDao{
+
+    private Connection connection;
+
+    /**
+     * Constructor that makes connection to the database with  hidden values needed to make the connection
+     */
+    public RoomTypesDaoSQLImpl(){
+        try{
+            String fieldPath = "src/dataBase.properties";
+            Properties pros = new Properties();
+            FileInputStream ip = new FileInputStream(fieldPath);
+            pros.load(ip);
+            this.connection = DriverManager.getConnection(pros.getProperty("url"), pros.getProperty("username"), pros.getProperty("password"));
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public RoomTypes getById(int id) {
         return null;
