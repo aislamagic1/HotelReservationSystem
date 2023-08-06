9a0cd76b7ea99d2732257bfb49a02094f4883017
aislamagic1 <115921908+aislamagic1@users.noreply.github.com>
2023-08-06 21:07:55 +0200

    Added constructor for RoomsDaoSQLImpl

--- src/main/java/ba/unsa/etf/rpr/dao/RoomsDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/RoomsDaoSQLImpl.java
@@ -2,9 +2,31 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Rooms;
 
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.List;
+import java.util.Properties;
 
 public class RoomsDaoSQLImpl implements RoomsDao{
+
+    private Connection connection;
+
+    /**
+     * Constructor that makes connection to the database with  hidden values needed to make the connection
+     */
+    public RoomsDaoSQLImpl(){
+        try{
+            String fieldPath = "src/dataBase.properties";
+            Properties pros = new Properties();
+            FileInputStream ip = new FileInputStream(fieldPath);
+            pros.load(ip);
+            this.connection = DriverManager.getConnection(pros.getProperty("url"), pros.getProperty("username"), pros.getProperty("password"));
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public Rooms getById(int id) {
         return null;
