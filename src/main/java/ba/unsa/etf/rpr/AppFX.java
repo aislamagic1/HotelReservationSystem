c426f17ffa0b15f9908c07c7f40a9a88cf40e577
aislamagic1 <115921908+aislamagic1@users.noreply.github.com>
2023-08-08 22:43:23 +0200

    Added main AppFX application

---
+++ src/main/java/ba/unsa/etf/rpr/AppFX.java
@@ -0,0 +1,12 @@
+package ba.unsa.etf.rpr;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppFX extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
