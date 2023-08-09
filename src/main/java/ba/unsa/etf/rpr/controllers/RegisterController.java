7c97d338eeb9dd23985b830887d9a7b38ce1f505
aislamagic1 <115921908+aislamagic1@users.noreply.github.com>
2023-08-09 18:42:43 +0200

    Added listeners for firstName and lastName in RegisterController

--- src/main/java/ba/unsa/etf/rpr/controllers/RegisterController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RegisterController.java
@@ -1,10 +1,12 @@
 package ba.unsa.etf.rpr.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,6 +15,18 @@ import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class RegisterController {
     public Button returnButton;
+    public TextField firstName;
+    public TextField lastName;
+    public TextField eMail;
+
+    public TextField password;
+
+    @FXML
+    void initialize(){
+        firstName.textProperty().addListener((observableValue, odlVal, newVal) ->{});
+
+        lastName.textProperty().addListener((observableValue, odlVal, newVal) ->{});
+    }
 
     public void returnClick(ActionEvent actionEvent) throws IOException {
         Stage stage = new Stage();
