e20014bc9899aff41643c68f3b9ef7ca5dcd9153
aislamagic1 <115921908+aislamagic1@users.noreply.github.com>
2023-08-10 18:20:12 +0200

    Implemented logout button in MainMenuController

--- src/main/java/ba/unsa/etf/rpr/controllers/MainMenuController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/MainMenuController.java
@@ -1,4 +1,29 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
 public class MainMenuController {
+    public Button logoutButton;
+
+    public void logoutButtonClick(ActionEvent actionEvent) throws IOException {
+        Stage stage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
+        Parent root = loader.load();
+        stage.setTitle("Hotel Reservation System");
+        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setResizable(false);
+        stage.show();
+        Stage primaryStage = (Stage) logoutButton.getScene().getWindow();
+        primaryStage.hide();
+        System.out.println("Returning to login screen.");
+    }
 }
