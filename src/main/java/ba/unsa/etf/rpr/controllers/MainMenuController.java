e8f6c6308e966cd534bc85a6c5ef07ceb986f74d
aislamagic1 <115921908+aislamagic1@users.noreply.github.com>
2023-08-10 18:29:24 +0200

    Added logout option in menu bar

--- src/main/java/ba/unsa/etf/rpr/controllers/MainMenuController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/MainMenuController.java
@@ -32,4 +32,17 @@ public class MainMenuController {
         primaryStage.hide();
         System.out.println("Returning to login screen.");
     }
+
+    public void logoutMenuClick(ActionEvent actionEvent) throws IOException {
+        Stage stage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
+        Parent root = loader.load();
+        stage.setTitle("Hotel Reservation System");
+        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setResizable(false);
+        stage.show();
+        Stage primaryStage = (Stage) logoutButton.getScene().getWindow();
+        primaryStage.hide();
+        System.out.println("Returning to login screen.");
+    }
 }
