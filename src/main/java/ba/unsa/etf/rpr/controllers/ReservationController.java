6f94127f3e231c04e7655c2563aac864d33c36ba
aislamagic1 <115921908+aislamagic1@users.noreply.github.com>
2023-08-12 01:15:34 +0200

    Implemented show price in reservation window

--- src/main/java/ba/unsa/etf/rpr/controllers/ReservationController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ReservationController.java
@@ -2,10 +2,12 @@ package ba.unsa.etf.rpr.controllers;
 
 import ba.unsa.etf.rpr.domain.RoomTypes;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,12 +18,15 @@ import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 public class ReservationController {
 
     public Button cancelButton;
+    public Label priceLabel;
     private RoomTypes roomType;
 
     public void setRoomType(RoomTypes roomType){
         this.roomType = roomType;
     }
-
+    public void setRoomPrice(String price){
+        priceLabel.setText(price);
+    }
 
     public void cancelButtonClick(ActionEvent actionEvent) throws IOException {
         Stage primaryStage = (Stage) cancelButton.getScene().getWindow();
