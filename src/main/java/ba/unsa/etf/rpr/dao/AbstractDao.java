93bb4bba4c45eedae65835e3b68cdea11618a78f
aislamagic1 <115921908+aislamagic1@users.noreply.github.com>
2023-08-16 00:03:53 +0200

    Added in AbstractDao

--- src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -2,6 +2,11 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Idable;
 
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
 
 /**
  * Abstract class that implements core DAO CRUD methods for every entity
@@ -9,5 +14,23 @@ import ba.unsa.etf.rpr.domain.Idable;
  */
 
 public abstract class AbstractDao<T extends Idable> implements Dao<T> {
+    private Connection connection;
+    private String tableName;
+
+    /**
+     * Constructor that makes connection to the database with  hidden values needed to make the connection
+     */
+    public AbstractDao(String tableName){
+        try{
+            this.tableName = tableName;
+            String fieldPath = "src/dataBase.properties";
+            Properties pros = new Properties();
+            FileInputStream ip = new FileInputStream(fieldPath);
+            pros.load(ip);
+            this.connection = DriverManager.getConnection(pros.getProperty("url"), pros.getProperty("username"), pros.getProperty("password"));
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 
 }
