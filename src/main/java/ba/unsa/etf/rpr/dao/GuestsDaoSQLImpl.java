fea3f683821320223028802e1aa36083f9c9cf32
aislamagic1 <115921908+aislamagic1@users.noreply.github.com>
2023-08-16 18:42:12 +0200

    Refactored GuestsDaoSQLImpl

--- src/main/java/ba/unsa/etf/rpr/dao/GuestsDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/GuestsDaoSQLImpl.java
@@ -4,124 +4,23 @@ import ba.unsa.etf.rpr.domain.Guests;
 
 import java.io.FileInputStream;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-import java.util.Properties;
+import java.util.*;
 
-public class GuestsDaoSQLImpl implements GuestsDao{
+public class GuestsDaoSQLImpl extends AbstractDao<Guests> implements GuestsDao{
 
-    private Connection connection;
-
-
-    /**
-     * Constructor that makes connection to the database with  hidden values needed to make the connection
-     */
-    public GuestsDaoSQLImpl(){
-        try{
-            String fieldPath = "src/dataBase.properties";
-            Properties pros = new Properties();
-            FileInputStream ip = new FileInputStream(fieldPath);
-            pros.load(ip);
-            this.connection = DriverManager.getConnection(pros.getProperty("url"), pros.getProperty("username"), pros.getProperty("password"));
-        }catch(Exception e){
-            e.printStackTrace();
-        }
+    public GuestsDaoSQLImpl() {
+        super("Guests");
     }
 
+
     @Override
-    public Guests getById(int id) {
-        String query = "SELECT * FROM Guests WHERE Guest_id = ?";
-        try {
-            PreparedStatement stmt = this.connection.prepareStatement(query);
-            stmt.setInt(1, id);
-            ResultSet rs = stmt.executeQuery();
-            if(rs.next()){
-                Guests guest = new Guests();
-                guest.setId(rs.getInt("Guest_id"));
-                guest.setFirstName(rs.getString("First_name"));
-                guest.setLastName(rs.getString("Last_name"));
-                guest.setEmail(rs.getString("eMail"));
-                guest.setPassword(rs.getString("password"));
-                rs.close();
-                return guest;
-            }else{
-                return null;
-            }
-        }catch (SQLException e){
-            e.printStackTrace();
-        }
+    public Guests row2object(ResultSet rs){
         return null;
     }
 
     @Override
-    public Guests add(Guests item) {
-        String insert = "INSERT INTO Guests (First_name, Last_name, eMail, password) VALUES (?, ?, ?, ?)";
-        try{
-            PreparedStatement stmt = this.connection.prepareStatement(insert, Statement.RETURN_GENERATED_KEYS);
-            stmt.setString(1, item.getFirstName());
-            stmt.setString(2, item.getLastName());
-            stmt.setString(3, item.getEmail());
-            stmt.setString(4, item.getPassword());
-            stmt.executeUpdate();
-            return item;
-        }catch (SQLException e){
-            e.printStackTrace();
-        }
+    public Map<String, Object> object2row(Guests object) {
         return null;
-    }
-
-    @Override
-    public Guests update(Guests item) {
-        String update = "UPDATE Guests SET First_name = ?, Last_name = ?, eMail = ?, password = ? WHERE Guest_id = ?";
-        try {
-            PreparedStatement stmt = this.connection.prepareStatement(update, Statement.RETURN_GENERATED_KEYS);
-            stmt.setString(1, item.getFirstName());
-            stmt.setString(2, item.getLastName());
-            stmt.setString(3, item.getEmail());
-            stmt.setString(4, item.getPassword());
-            stmt.setInt(5, item.getId());
-            stmt.executeUpdate();
-            return item;
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    @Override
-    public void delete(int id) {
-        String query = "DELETE FROM Guests WHERE Guest_id = ?";
-        try {
-            PreparedStatement stmt = this.connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
-            stmt.setInt(1, id);
-            stmt.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public List<Guests> getAll() {
-        String query = "SELECT * FROM Guests";
-        List<Guests> guests = new ArrayList<Guests>();
-        try{
-            PreparedStatement stmt = this.connection.prepareStatement(query);
-            ResultSet rs = stmt.executeQuery();
-            while (rs.next()){
-                Guests guest = new Guests();
-                guest.setId(rs.getInt("Guest_id"));
-                guest.setFirstName(rs.getString("First_name"));
-                guest.setLastName(rs.getString("Last_name"));
-                guest.setEmail(rs.getString("eMail"));
-                guest.setPassword(rs.getString("password"));
-                guests.add(guest);
-            }
-            rs.close();
-        }catch (SQLException e){
-            e.printStackTrace();
-        }
-        return guests;
     }
 
 
@@ -135,7 +34,7 @@ public class GuestsDaoSQLImpl implements GuestsDao{
     public Guests getGuestByEmailAndPassword(String email, String password) {
         String query = "SELECT * FROM Guests WHERE eMail = ? AND password = ?";
         try{
-            PreparedStatement stmt = this.connection.prepareStatement(query);
+            PreparedStatement stmt = getConnection().prepareStatement(query);
             stmt.setString(1, email);
             stmt.setString(2, password);
             ResultSet rs = stmt.executeQuery();
