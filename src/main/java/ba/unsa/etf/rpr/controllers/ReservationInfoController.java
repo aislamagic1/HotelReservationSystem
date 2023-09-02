2703ccd30eae0af0a596ca5c87d266463388fb05
aislamagic1 <115921908+aislamagic1@users.noreply.github.com>
2023-09-02 15:48:32 +0200

    Added attributes and display methods in ReservationInfoController

--- src/main/java/ba/unsa/etf/rpr/controllers/ReservationInfoController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ReservationInfoController.java
@@ -1,4 +1,27 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.scene.control.Label;
+
 public class ReservationInfoController {
+    public Label roomNumber;
+    public Label roomType;
+    public Label arrivalDate;
+    public Label checkOutDate;
+
+    public void displayRoomNumber(String val){
+        roomNumber.setText(val);
+    }
+
+    public void displayRoomType(String val){
+        roomType.setText(val);
+    }
+
+    public void displayArrivalDate(String val){
+        arrivalDate.setText(val);
+    }
+
+    public void displayCheckOutDate(String val){
+        checkOutDate.setText(val);
+    }
+
 }
