31ee9e8b3cea4e8f56f7c715af2289213380fb6a
aislamagic1 <115921908+aislamagic1@users.noreply.github.com>
2023-09-03 01:14:30 +0200

    Added close window method in ReservationInfoController

--- src/main/java/ba/unsa/etf/rpr/controllers/ReservationInfoController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ReservationInfoController.java
@@ -1,12 +1,20 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class ReservationInfoController {
     public Label roomNumber;
     public Label roomType;
     public Label arrivalDate;
     public Label checkOutDate;
+    public Button closeButton;
 
     public void displayRoomNumber(String val){
         roomNumber.setText(val);
@@ -22,6 +30,11 @@ public class ReservationInfoController {
 
     public void displayCheckOutDate(String val){
         checkOutDate.setText(val);
+    }
+
+    private void closeWindow(){
+        Stage primaryStage = (Stage) closeButton.getScene().getWindow();
+        primaryStage.hide();
     }
 
 }
